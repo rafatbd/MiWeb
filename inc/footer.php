fdafbb061390dc59d31cc0c9f2c40d874d9bf426
rafatbd <rafatinfo4@gmail.com>
2018-02-22 21:05:16 +0100

    inicio

--- inc/footer.php
+++ inc/footer.php
@@ -48,7 +48,7 @@
 			<h2>Card Image</h2>
 			<p>Image at the top (card-img-top):</p>
 			<div class="card" style="width:400px">
-		    	<img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%">
+		    	<img class="card-img-top" src="1550.png" alt="Card image" style="width:100%">
 		    	<div class="card-body">
 		      		<h4 class="card-title">John Doe</h4>
 		      		<p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
@@ -64,7 +64,7 @@
 		      		<p class="card-text">Some example text some example text. Jane Doe is an architect and engineer</p>
 		      		<a href="#" class="btn btn-primary">See Profile</a>
 		    	</div>
-		    	<img class="card-img-bottom" src="img_avatar6.png" alt="Card image" style="width:100%">
+		    	<img class="card-img-bottom" src="oj.png" alt="Card image" style="width:100%">
 		  </div>
 		</div>
 		<form id="" class="" >
