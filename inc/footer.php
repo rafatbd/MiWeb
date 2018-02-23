130d979d3fdc93ae2eb52e6e7b818f49cd5a531d
rafatbd <rafatinfo4@gmail.com>
2018-02-23 12:56:48 +0100

    inicio

--- inc/footer.php
+++ inc/footer.php
@@ -1,14 +1,32 @@
 		<!-- TODO:contenido body -->
 	<div class="row">
 		<div class="col-6">
-			<h1>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-			</h1>	
+			<a href="#">
+				<nav>
+				  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+				    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Home</a>
+				    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</a>
+				    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</a>
+				  </div>
+				</nav>
+				<div class="tab-content" id="nav-tabContent">
+				  <div class="tab-pane fade show active " id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+				  	<a href="index.php">
+				  		<img src="img/croissant.png">	
+				  	</a>
+				  </div>
+				  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+				  	<a href="index.php">
+				  		<img src="img/ajuntament.png">	
+				  	</a>
+				  </div>
+				  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+				  	<a href="index.php">
+				  		<img src="img/nubes.jpg">	
+				  	</a>
+				  </div>
+				</div>
+			</a>
 		</div>
 		<div class="col-6">
 			<h1>
@@ -66,6 +84,7 @@
 		<div class="col-6">
 		</div>
 	</div>
+		
 		<!-- <div id="content2" class="container-fluid"> -->
 			
 		<!-- </div> -->
@@ -97,7 +116,7 @@
 		    	<img class="card-img-bottom" src="oj.png" alt="Card image" style="width:100%">
 		  </div>
 		</div> -->
-
+		
 		<form id="" class="" >
 			<div class="form-group">
     			<label for="exampleInputEmail1">Email address</label>
